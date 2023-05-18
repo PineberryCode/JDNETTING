704398975d1521b58ebab42a021b01757d95d55c
PineberryCode <applepie090902@gmail.com>
2023-05-18 12:09:24 -0500

    First commit

---
+++ General/DefaultClass.java
@@ -0,0 +1,7 @@
+package General;
+
+public interface DefaultClass {
+    static int[] IPClassA = {0,126};
+    static int[] IPClassB = {127,191};
+    static int[] IPClassC = {192,223};
+}
---
+++ General/DefaultText.java
@@ -0,0 +1,7 @@
+package General;
+
+public class DefaultText {
+    public void TextLN(String str) {System.out.println(str);}
+    public void TextNonLN(String str) {System.out.print(str);}
+    public static void Jump() {System.out.println();}
+}
