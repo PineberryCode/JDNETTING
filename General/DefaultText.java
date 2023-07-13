1af1ea82ad3e60d8975c952ba50f33b753ead34b
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-07-13 08:14:23 -0500

    Delete General directory

--- General/DefaultText.java
+++
@@ -1,7 +0,0 @@
-package General;
-
-public class DefaultText {
-    public void TextLN(String str) {System.out.println(str);}
-    public void TextNonLN(String str) {System.out.print(str);}
-    public static void Jump() {System.out.println();}
-}
