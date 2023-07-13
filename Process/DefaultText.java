85caf9b29e4543bfcd3bb08bd9016b69e5338a28
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-07-13 08:14:45 -0500

    Delete Process directory

--- Process/DefaultText.java
+++
@@ -1,7 +0,0 @@
-package Process;
-
-public class DefaultText {
-    public void TextLN(String str) {System.out.println(str);}
-    public void TextNonLN(String str) {System.out.print(str);}
-    public static void Jump() {System.out.println();}
-}
