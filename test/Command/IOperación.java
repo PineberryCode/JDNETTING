1a7ae6d5de5027af4ced2fdc684364b0ee75d4fe
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-07-13 08:14:57 -0500

    Delete test directory

--- test/Command/IOperación.java
+++
@@ -1,7 +0,0 @@
-package test.Command;
-
-//Command
-@FunctionalInterface
-public interface IOperación {
-    void execute();
-}
--- test/Interpreter/Main.java
+++
@@ -1,8 +0,0 @@
-package test.Interpreter;
-    public class Main {
-        public static void main(String[] args) {
-            Calculator calculator = new Calculator();
-            calculator.interpretCommand("+ 5");  // Resultado: 5
-            calculator.interpretCommand("- 3");  // Resultado: 2
-        }
-}
