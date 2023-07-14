2b295800578de135b2aa0cd92f6618e2707da106
PineberryCode <applepie090902@gmail.com>
2023-07-14 18:53:38 -0500

    adding: sorted qh, host required by subnet and prefix

--- src/Dev.java
+++ src/Dev.java
@@ -4,28 +4,67 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Dev {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner (System.in);
         List<String> list = new ArrayList<>();
+        List<Integer> listHostRequired = new ArrayList<>();
+        List<Integer> listQH = new ArrayList<>();
+        List<Integer> listPrefix = new ArrayList<>();
         int k = 1;
         var str = Arrays.toString(args);
 
         if (str.contains("-ip") && str.contains("-qh")) {
-            list.add(args[0]);
             if (args[0] != null) {
+                list.add(args[0]);
                 System.out.println("Added IP address");
-                for (int x=0; x<list.size(); x++) {
+                /*for (int x=0; x<list.size(); x++) {
                     System.out.print(list.get(x));
+                }*/
+            }
+            System.out.println("#### Write the quantity host by subnet ###");
+            while (k <= Integer.parseInt(args[2])) {
+                System.out.print("Subnet00"+k+"=> ");
+                int qh = Integer.parseInt(scn.next());
+                listQH.add(qh);
+                k++;
+            }
+
+            Comparator<Integer> descendingComparator = (a, b) -> b.compareTo(a);
+            Collections.sort(listQH, descendingComparator);
+
+            for (int f=0; f < listQH.size(); f++) {
+                System.out.println(listQH.get(f));
+            }
+            for (int o=0; o<listQH.size(); o++) {
+                int i = 1;
+                while (true) {
+                    int formula = (int) (Math.pow(2, i) - 2);
+                    if (formula >= listQH.get(o)) {
+                        listHostRequired.add(formula);
+                        
+                        int prefix = 32 - i;
+                        
+                        listPrefix.add(prefix);
+                        i = 1;
+                        break;
+                    }
+                    i++;
                 }
             }
-            do {
-                System.out.print("Subnet00"+k+"=> "+ k+" ".concat(scn.nextLine()));
-                k++;
-            } while (k <= Integer.parseInt(args[2]));
+            for (int z=0; z<listHostRequired.size(); z++) {
+                System.out.println(listHostRequired.get(z));
+            }
+            for (int a=0; a<listPrefix.size(); a++) {
+                System.out.println(listPrefix.get(a));
+            }
+            scn.close();
         }
     }
 }
---
+++ src/model/VLSM.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VLSM {
+
+    List<String> myListIPAddress;
+    List<Integer> myListQuantityHost;
+    
+    public VLSM() {myListIPAddress = new ArrayList<>();}
+}
