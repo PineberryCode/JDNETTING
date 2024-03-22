bdb792e1aed266ff54a5e176d08197b87444a68c
PineberryCode <applepie090902@gmail.com>
2024-03-22 11:29:53 -0500

    running.sh modified

--- running.sh
+++ running.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#create a bin dir
-mkdir bin
-
 #create a bin
-find src -name "*.java" -print | xargs javac -d bin
+if [ ! -d "/bin" ];
+then
+    #create a bin dir
+    mkdir bin
+
+    find src -name "*.java" -print | xargs javac -d bin
+else
+    find src -name "*.java" -print | xargs javac -d bin
+fi
 
 #Create an alias compiles 'java -cp bin Dev'
 alias jdnet='java -cp bin Dev'
