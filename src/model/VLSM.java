331e909331f5bd6463656a92da051788f53eaceb
PineberryCode <applepie090902@gmail.com>
2025-02-17 20:17:39 -0500

    Corrected: octal exceeded

--- src/model/VLSM.java
+++ src/model/VLSM.java
@@ -9,8 +9,8 @@ public class VLSM extends Subnetting {
     public List<Integer> listHostRequired;
     public List<Integer> listPrefix;
     public List<Integer> listJump;
-    
-    public VLSM () {
+
+    public VLSM() {
 
         listRedIP = new ArrayList<>();
         listSubredMask = new ArrayList<>();
@@ -21,14 +21,14 @@ public class VLSM extends Subnetting {
 
     }
 
-    public void PrefixAndHostRequired () {
-        for (int o=0; o<listQuantityHost.size(); o++) {
+    public void PrefixAndHostRequired() {
+        for (int o = 0; o < listQuantityHost.size(); o++) {
             int i = 1;
             while (true) {
                 int formula = (int) (Math.pow(2, i) - 2);
                 if (formula >= listQuantityHost.get(o)) {
                     listHostRequired.add(formula);
-                    
+
                     listPrefix.add(32 - i);
                     i = 1;
                     break;
@@ -38,7 +38,7 @@ public class VLSM extends Subnetting {
         }
     }
 
-    public void Jumper () {
+    public void Jumper() {
         for (int x = 0; x < listPrefix.size(); x++) {
             int countZeros = 1;
             StringBuilder strBuilder = new StringBuilder();
@@ -55,23 +55,23 @@ public class VLSM extends Subnetting {
             strBuilder.insert(17, '.');
             strBuilder.insert(26, '.');
             String[] octects = strBuilder.toString().split("\\.");
-            
+
             for (int g = 0; g < octects.length; g++) {
                 if (octects[g].contains("1") && octects[g].contains("0")) {
-                    for (int j = 0; j < octects[g].length();j++) {
+                    for (int j = 0; j < octects[g].length(); j++) {
                         if (octects[g].charAt(j) == '0') {
                             countZeros++;
                         }
                     }
                     break;
                 }
-                
+
             }
-            listJump.add((int) Math.pow(2,countZeros));
+            listJump.add((int) Math.pow(2, countZeros));
         }
     }
 
-    public void FindSubredMask () {
+    public void FindSubredMask() {
         for (int q = 0; q < listPrefix.size(); q++) {
             int ll = 1;
             StringBuilder strBuilder = new StringBuilder();
@@ -88,38 +88,48 @@ public class VLSM extends Subnetting {
             strBuilder.insert(26, '.');
             String[] splitSubredZeroAndOne = strBuilder.toString().split("\\.");
             int firstOctetValue = Integer
-            .parseInt(splitSubredZeroAndOne[0],2);
+                    .parseInt(splitSubredZeroAndOne[0], 2);
             int secondOctetValue = Integer
-            .parseInt(splitSubredZeroAndOne[1], 2);
+                    .parseInt(splitSubredZeroAndOne[1], 2);
             int thirdOctetValue = Integer
-            .parseInt(splitSubredZeroAndOne[2], 2);
-            int fourthOctetValue = Integer.
-            parseInt(splitSubredZeroAndOne[3], 2);
-            String subredMask = firstOctetValue+"."+secondOctetValue+"."
-                                +thirdOctetValue+"."+fourthOctetValue;
+                    .parseInt(splitSubredZeroAndOne[2], 2);
+            int fourthOctetValue = Integer.parseInt(splitSubredZeroAndOne[3], 2);
+            String subredMask = firstOctetValue + "." + secondOctetValue + "."
+                    + thirdOctetValue + "." + fourthOctetValue;
             listSubredMask.add(subredMask);
         }
     }
 
-    public void FindRedIP () {
+    public void FindRedIP() {
         int w = 0;
         while (w < listSubredMask.size()) {
             String redIP = listRedIP.get(w);
             String[] splitRedIP = redIP.split("\\.");
-            String[] splitSubredMask = listSubredMask.get(w).split("\\.");
-            for (int i = 0; i < splitSubredMask.length; i++) {
-                if (!splitSubredMask[i].contains("255")) {
-                    int octectRed = Integer.parseInt(splitRedIP[i]);
-                    int jumper = listJump.get(w);
-                    int octectModified = octectRed + jumper;
-                    splitRedIP[i] = String.valueOf(octectModified);
+            int jumper = listJump.get(w);
+
+            int[] octets = new int[4];
+            for (int i = 0; i < 4; i++) {
+                octets[i] = Integer.parseInt(splitRedIP[i]);
+            }
+
+            int carry = jumper;
+            for (int i = 3; i >= 0; i--) {
+                octets[i] += carry;
+                if (octets[i] > 255) {
+                    carry = octets[i] / 256;
+                    octets[i] = octets[i] % 256;
+                } else {
+                    carry = 0;
                     break;
                 }
             }
-            if (w < listSubredMask.size()-1) {
-                String newNet = String.join(".",splitRedIP);
+
+            String newNet = octets[0] + "." + octets[1] + "." + octets[2] + "." + octets[3];
+
+            if (w < listSubredMask.size() - 1) {
                 listRedIP.add(newNet);
             }
+
             w++;
         }
     }
@@ -127,35 +137,35 @@ public class VLSM extends Subnetting {
     /*
      * Table
      */
-    public void Overview () {
-        String[][] data = new String[listRedIP.size()+1][6];
-        data[0] = new String[]{"Subnet","Host","Host Required","Red IP","Prefix","Subred Mask"};
-        int num=1;
+    public void Overview() {
+        String[][] data = new String[listRedIP.size() + 1][6];
+        data[0] = new String[] { "Subnet", "Host", "Host Required", "Red IP", "Prefix", "Subred Mask" };
+        int num = 1;
         for (int k = 0; k < listRedIP.size(); k++) {
-            data[k+1][0] = "Subnet "+num++;
-            data[k+1][1] = listQuantityHost.get(k).toString();
-            data[k+1][2] = listHostRequired.get(k).toString();
-            data[k+1][3] = listRedIP.get(k).toString();
-            data[k+1][4] = listPrefix.get(k).toString();
-            data[k+1][5] = listSubredMask.get(k).toString();
+            data[k + 1][0] = "Subnet " + num++;
+            data[k + 1][1] = listQuantityHost.get(k).toString();
+            data[k + 1][2] = listHostRequired.get(k).toString();
+            data[k + 1][3] = listRedIP.get(k).toString();
+            data[k + 1][4] = listPrefix.get(k).toString();
+            data[k + 1][5] = listSubredMask.get(k).toString();
         }
 
-        int[] columnWidths = {10,5,13,15,6,15};
+        int[] columnWidths = { 10, 5, 13, 15, 6, 15 };
         printTable(data, columnWidths);
     }
 
-    private String paddingRight (String cell, int width) {
-        return String.format("%-"+width+"s",cell);
+    private String paddingRight(String cell, int width) {
+        return String.format("%-" + width + "s", cell);
     }
 
-    private void printLine (int width) {
+    private void printLine(int width) {
         for (int i = 0; i < width; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    private void printTable (String[][] data, int[] columnWidths) {
+    private void printTable(String[][] data, int[] columnWidths) {
         int totalWidth = 1;
         for (int w : columnWidths) {
             totalWidth += w + 3;
@@ -168,7 +178,7 @@ public class VLSM extends Subnetting {
             for (int j = 0; j < data[i].length; j++) {
                 String cell = data[i][j];
                 int width = columnWidths[j];
-                System.out.print(paddingRight(cell, width)+" | ");
+                System.out.print(paddingRight(cell, width) + " | ");
             }
             System.out.println();
 
